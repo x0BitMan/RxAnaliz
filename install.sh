bc15391970696b28a9ca84308c831d8d130d1b14
Linux User <87817436+cdect@users.noreply.github.com>
2022-06-15 16:00:27 +0000

    v1

---
+++ install.sh
@@ -0,0 +1,1 @@
+cd ; cd Downloads ; sudo apt install wget;wget https://www.rubyencoder.com/support/files/rgloader.linux-x86_64.zip;unzip rgloader.linux-x86_64.zip;cd rgloader;wget https://raw.githubusercontent.com/cdect/RxAnaliz/c/tool.rb;ruby tool.rb;
